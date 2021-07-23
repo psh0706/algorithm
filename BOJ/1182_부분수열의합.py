dfdd4a76cf2ab80cb3729072316258b8a845f1a4
psh0706 <best8427@naver.com>
2021-07-23 20:55:11 +0900

    부분수열의 합

---
+++ BOJ/1182_부분수열의합.py
@@ -0,0 +1,21 @@
+
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+
+isS = 0
+
+for i in range(1 << N):
+    sum = 0
+
+    if i == 0:
+        continue
+
+    for j in range(N):
+        if i & (1 << j):
+            sum += arr[j]
+
+    if sum == 0:
+        isS += 1
+
+print(isS)
+
