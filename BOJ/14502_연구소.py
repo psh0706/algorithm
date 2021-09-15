1df6adbd2c06fac011028e374e05c5c775b8fb69
psh0706 <best8427@naver.com>
2021-09-16 01:28:25 +0900

    BOJ 14502 연구소 (재귀+BFS) 수정

--- BOJ/14502_연구소.py
+++ BOJ/14502_연구소.py
@@ -1,5 +1,6 @@
 import sys
 from copy import deepcopy
+from collections import deque
 
 
 def check():
@@ -7,24 +8,24 @@ def check():
     clone = deepcopy(MAP)
     global maxi
     safeArea = area - 3
+    q = deque()
 
     for virus in virusIndex:
-        q = [virus]
+        q.append(virus)
         checkVisit[virus[0]][virus[1]] = True
 
-        while len(q) != 0:
-            n = q.pop(0)
+    while len(q) != 0:
+        n = q.popleft()
 
-            for d in delta:
-                dx = n[0] + d[0]
-                dy = n[1] + d[1]
+        for d in delta:
+            dx = n[0] + d[0]
+            dy = n[1] + d[1]
 
-                if 0 <= dx < N and 0 <= dy < M and not checkVisit[dx][dy]:
-                    if clone[dx][dy] == 0:
-                        checkVisit[dx][dy] = True
-                        clone[dx][dy] = 2
-                        safeArea -= 1
-                        q.append([dx, dy])
+            if 0 <= dx < N and 0 <= dy < M and not checkVisit[dx][dy] and clone[dx][dy] == 0:
+                checkVisit[dx][dy] = True
+                clone[dx][dy] = 2
+                safeArea -= 1
+                q.append([dx, dy])
 
     if maxi < safeArea:
         maxi = safeArea
@@ -34,21 +35,18 @@ def check():
 def recursion(depth, x, y):
     if depth == 3:
         check()
-        # for m in MAP:
-        #     print(m)
-        # print('-----------')
         return
 
-    while x != N and y != M:
-        if MAP[x][y] == 0:
+    while x < N:
+        if 0 <= x < N and 0 <= y < M and MAP[x][y] == 0:
             MAP[x][y] = 1
             recursion(depth + 1, x, y+1)
             MAP[x][y] = 0
-        y += 1
         if y == M:
             x += 1
             y -= y
-
+        else:
+            y += 1
 
 
 if __name__ == '__main__':
@@ -67,8 +65,8 @@ if __name__ == '__main__':
 
     visit = [[False for _ in range(M)] for _ in range(N)]
     delta = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-    answerList = []
     maxi = 0
 
     recursion(0, 0, 0)
+
     print(maxi)
