d6e5e1ba90e473126ab6258fea607cd74f706010
psh0706 <best8427@naver.com>
2021-10-17 00:34:47 +0900

    Bubble Sort (거품정렬) 개선 알고리즘 2

---
+++ Sort/Bubble_Sort_개선2.py
@@ -0,0 +1,20 @@
+# 입력 받을 요소의 개수
+N = int(input())
+# 리스트 입력받기
+arr = list(map(int, input().split()))
+
+# k 를 이용하여 범위제한
+k = N
+while k > 0:
+    last = 0
+    # k 번째 인덱스까지 수행
+    for j in range(1, k):
+        if arr[j-1] > arr[j]:
+            temp = arr[j-1]
+            arr[j-1] = arr[j]
+            arr[j] = temp
+            last = j
+    # k 는 마지막으로 swap 된 인덱스
+    k = last
+
+print(arr)
