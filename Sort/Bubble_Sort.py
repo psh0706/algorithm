e065ac4a99006ceceedad353a127b0547574cb80
psh0706 <best8427@naver.com>
2021-10-16 21:46:59 +0900

    Bubble Sort (거품정렬) 구현

--- Sort/Bubble_Sort.py
+++ Sort/Bubble_Sort.py
@@ -3,8 +3,8 @@ N = int(input())
 # 리스트 입력받기
 arr = list(map(int, input().split()))
 
+# 각 턴을 N-1 번 시행한다 -> 길이 N인 리스트의 요소를 하나하나 fix 해 나간다.
 for _ in range(N - 1):
-    # N번 시행한다 -> 길이 N인 리스트의 요소를 하나하나 fix 해 나간다.
     for j in range(1, N):
         # 인접한 두 요소를 비교
         if arr[j - 1] > arr[j]:
