659ad3990d3e1b776ee5ff7ee4d0d74df8fa0cbe
psh0706 <best8427@naver.com>
2021-11-22 00:58:17 +0900

    BOJ 18428 감시피하기 (재귀+구현) - 수정

--- BOJ/18428_감시피하기.py
+++ BOJ/18428_감시피하기.py
@@ -8,18 +8,18 @@ MAP = []
 
 def isCatch(t, dic):
     if dic == "u":
-        s = dict({'x': 0, 'y': 0})
-        o = dict({'x': 0, 'y': 0})
+        s = dict({'x': -1, 'y': -1})
+        o = dict({'x': -1, 'y': -1})
         for item in S:
-            if item['y'] == t['y']:
+            if item['y'] == t['y'] and item['x'] < t['x']:
                 if item['x'] > s['x']:
                     s = item
         for item in O:
-            if item['y'] == t['y']:
+            if item['y'] == t['y'] and item['x'] < t['x']:
                 if item['x'] > o['x']:
                     o = item
 
-        if s['x'] < o['x'] < t['x']:
+        if (s['x'] == -1 and s['y'] == -1) or s['x'] < o['x'] < t['x']:
             return True
         else:
             return False
@@ -27,31 +27,31 @@ def isCatch(t, dic):
         s = dict({'x': N, 'y': N})
         o = dict({'x': N, 'y': N})
         for item in S:
-            if item['y'] == t['y']:
+            if item['y'] == t['y'] and item['x'] > t['x']:
                 if item['x'] < s['x']:
                     s = item
         for item in O:
-            if item['y'] == t['y']:
+            if item['y'] == t['y'] and item['x'] > t['x']:
                 if item['x'] < o['x']:
                     o = item
 
-        if s['x'] > o['x'] > t['x']:
+        if (s['x'] == N and s['y'] == N) or s['x'] > o['x'] > t['x']:
             return True
         else:
             return False
     elif dic == "l":
-        s = dict({'x': 0, 'y': 0})
-        o = dict({'x': 0, 'y': 0})
+        s = dict({'x': -1, 'y': -1})
+        o = dict({'x': -1, 'y': -1})
         for item in S:
-            if item['x'] == t['x']:
+            if item['x'] == t['x'] and item['y'] < t['y']:
                 if item['y'] > s['y']:
                     s = item
         for item in O:
-            if item['x'] == t['x']:
+            if item['x'] == t['x'] and item['y'] < t['y']:
                 if item['y'] > o['y']:
                     o = item
 
-        if s['y'] < o['y'] < t['y']:
+        if (s['x'] == -1 and s['y'] == -1) or s['y'] < o['y'] < t['y']:
             return True
         else:
             return False
@@ -59,15 +59,15 @@ def isCatch(t, dic):
         s = dict({'x': N, 'y': N})
         o = dict({'x': N, 'y': N})
         for item in S:
-            if item['x'] == t['x']:
+            if item['x'] == t['x'] and item['y'] > t['y']:
                 if item['y'] < s['y']:
                     s = item
         for item in O:
-            if item['x'] == t['x']:
+            if item['x'] == t['x'] and item['y'] > t['y']:
                 if item['y'] < o['y']:
                     o = item
 
-        if s['y'] > o['y'] > t['y']:
+        if (s['x'] == N and s['y'] == N) or s['y'] > o['y'] > t['y']:
             return True
         else:
             return False
@@ -77,12 +77,10 @@ def recur(depth):
     global flag
     if depth == 3:
         for t in T:
-            if isCatch(t, "u") and isCatch(t, "d") and isCatch(t, "l") and isCatch(t, "r"):
-                flag = True
-                print("YES")
+            if not isCatch(t, "u") or not isCatch(t, "d") or not isCatch(t, "l") or not isCatch(t, "r"):
                 return
-            else:
-                return
+        flag = True
+        return
 
     for x in range(N):
         for y in range(N):
@@ -109,10 +107,13 @@ for i in range(N):
 recur(0)
 if not flag:
     print("NO")
+else:
+    print("YES")
 
 
 """
 재귀 + 구현 문제
 Object 를 재귀로 3개 배치해주고 
 선생님의 시야에 학생이 보이는지를 체크해주었다.
++ 오류 수정 완료!
 """
