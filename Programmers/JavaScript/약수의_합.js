98112cf033b97c3fb2d929be98487ed8168e4fb2
psh0706 <best8427@naver.com>
2022-01-13 18:36:53 +0900

    PGMS 약수의 합 (LV1)(배열)

--- Programmers/JavaScript/약수의_합.js
+++ Programmers/JavaScript/약수의_합.js
@@ -2,5 +2,6 @@ function solution(n) {
     return Array.from({length: n+1}, (v, i) => i).reduce((sum, item)=> n%item === 0? sum+=item : sum);
 }
 
-// Array.from({length: n+1}, (v, i) => i) n 까지의 배열 만들기
+// Array.form 으로 수열 생성.
+// Array.from({length: n+1}, (v, i) => i) n 까지의 수열 만들기
 // reduce((sum, item)=> n%item === 0? sum+=item : sum) 0으로 나누어 떨어지는 숫자들만 더하기
